b0d906a93df3c40dc8cbb84615decfb5a8237d64
sunshine <sunshine_qingtian@yeah.net>
2019-05-22 16:43:04 +0800

    创建demo.php

---
+++ demo.php
@@ -0,0 +1,23 @@
+﻿<?php
+
+echo '大家坚持一下,马上就好了';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<style type="text/css">
+		*{
+			color: red;
+		}
+	</style>
+</head>
+<body>
+	<h1 onclick="window.alert('我是弹窗')">北京</h1>
+
+	<h2><?php echo '重庆'; ?></h2>
+</body>
+</html>
